9f8d07f103a7fc69bcb816af8675d702e9d5735a
karthikgotrala <6868408+kgotgit@users.noreply.github.com>
2020-10-16 01:09:38 -0500

    added app component changes

--- src/app/boxes/default-box/default-box.component.ts
+++ src/app/boxes/default-box/default-box.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DefaultBoxComponent implements OnInit {
 
+  boxes=[];
   constructor() { }
 
   ngOnInit(): void {
+
+    this.boxes.push("boxes "+this.boxes.length);
   }
 
 }
