1d51075404b2936ed4f68de5ced0089f518854ad
karthikgotrala <6868408+kgotgit@users.noreply.github.com>
2020-10-17 08:03:13 -0500

    default and onpush component changes

--- src/app/boxes/default-box/default-box.component.ts
+++ src/app/boxes/default-box/default-box.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-default-box',
   templateUrl: './default-box.component.html',
-  styleUrls: ['./default-box.component.css']
+  styleUrls: ['./default-box.component.css'],
 })
 export class DefaultBoxComponent implements OnInit {
 
--- src/app/boxes/on-push-box/on-push-box.component.ts
+++ src/app/boxes/on-push-box/on-push-box.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy, DoCheck, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-on-push-box',
   templateUrl: './on-push-box.component.html',
-  styleUrls: ['./on-push-box.component.css']
+  styleUrls: ['./on-push-box.component.css'],
+  changeDetection:ChangeDetectionStrategy.OnPush
 })
-export class OnPushBoxComponent implements OnInit {
+export class OnPushBoxComponent implements OnInit, DoCheck{
 
-  constructor() { }
+  _boxes:[];
+  _count=-1;
 
-  ngOnInit(): void {
+  @Input()
+  set boxes(boxes){
+    this._boxes=boxes;
+    this.setCount();
+  }
+  get boxes(){
+    return this._boxes;
+  }
+
+  constructor(private changeDetectorRef:ChangeDetectorRef) { }
+
+  ngOnInit(): void {}
+
+  setCount(){
+    this._count=Array.isArray(this._boxes)?this._boxes.length:0;
+  }
+
+  ngDoCheck(): void {
+    console.log("ngDoCheck");
+    if(this._boxes && this._boxes.length!=this._count){
+      this.changeDetectorRef.markForCheck();
+      this.setCount();
+      console.log("markForCheck");
+    }
+  }
+
+  noAction($event){
+    this.changeDetectorRef.markForCheck();
+    console.log("Detect Changes app-on-push-box");
   }
 
 }
